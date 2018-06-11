110461dbd6c83cd0d5e8f5abda11fbdc89b2f96e
Santiago Suarez Ordoñez <santiycr@gmail.com>
2018-06-10 17:26:07 -0700

    bump version

--- nameko_cachetools/__init__.py
+++ nameko_cachetools/__init__.py
@@ -2,5 +2,5 @@ from .nameko_cachetools import CachedRpcProxy, CacheFirstRpcProxy
 
 __author__ = 'Santiago Suarez Ordonez'
 __email__ = 'santiycr@gmail.com'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __all__ = ['CachedRpcProxy', 'CacheFirstRpcProxy']
